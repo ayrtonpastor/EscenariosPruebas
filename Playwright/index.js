54f5f8542bba9b0c2fd857af0f5875d299420ef8
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-05-09 15:19:36 -0500

    Se añaden escenarios 1 y 5 en playwright

---
+++ Playwright/index.js
@@ -0,0 +1,52 @@
+const e1 = require('./escenario1')
+const e5 = require('./escenario5')
+
+
+const url = 'http://localhost:2369/ghost/';
+
+//Función flecha asíncrona
+(async() => {
+    //Escenario 1 que recibe por parametro:
+    //@param url: constante global de url de pagina
+    //@param browser: el tipo de browser en el cual se hara la prueba "chromium, firefox o webkit"
+    //@param scenarioTag: tag del escenario
+    //@param wrongEmail: un correo erroneo para el login
+    //@param wrongPass: una contraseña erronea para el login
+    //@param correctEmail: un correo valido y existente para el login
+    //@param correctPass: una contraseña valida y correcta para el login
+    //@param loggedUserName: nombre del usuario registrado relacionado al correo valido y existente
+    //@param navSubRoute: ruta a la que se quiere navegar en la prueba
+    await e1.escenario1(
+            url,
+            'chromium',
+            'e1',
+            'asdf@uniandes.edu.co',
+            'fhadfshdasf',
+            'b@uniandes.edu.co',
+            '$0987654321.',
+            'Juan Carlos',
+            'staff'
+        )
+        //Escenario 5 que recibe por parametro:
+        //@param url: constante global de url de pagina
+        //@param browser: el tipo de browser en el cual se hara la prueba "chromium, firefox o webkit"
+        //@param scenarioTag: tag del escenario
+        //@param correctEmail: un correo valido y existente para el login
+        //@param correctPass: una contraseña valida y correcta para el login
+        //@param loggedUserName: nombre del usuario registrado relacionado al correo valido y existente
+        //@param navSubRoute: ruta a la que se quiere navegar en la prueba
+        //@param postTitle: titulo del post
+        //@param textPost: texto del post
+    await e5.escenario5(
+        url,
+        'chromium',
+        'e1',
+        'b@uniandes.edu.co',
+        '$0987654321.',
+        'Juan Carlos',
+        'posts',
+        'Post Nuevo Prueba',
+        'texto de prueba del post'
+    );
+
+})(); //Llamado propio de la función
---
+++ Playwright/page-objects.js
@@ -0,0 +1,112 @@
+const playwright = require('playwright');
+const expect = require('expect');
+let rutaPadre
+let numeroCaptura = 0
+exports.navegarUrl = async(page, url) => {
+    page = page[0]
+    rutaPadre = url
+    await page.goto(url)
+}
+
+exports.realizarLogin = async(page, email, password) => {
+    page = page[0]
+    const emailInput = '#ember8'
+    const passInput = '#ember10'
+    await page.click(`css=${emailInput}`)
+    await page.fill(`css=${emailInput}`, email)
+    await page.click(`css=${passInput}`)
+    await page.fill(`css=${passInput}`, password)
+    await page.click(`css=${emailInput}`)
+    await page.click(`css=${passInput}`)
+}
+
+exports.construirBrowser = async(browserType) => {
+    numeroCaptura = 0;
+    const browser = await playwright[browserType].launch({ headless: false });
+    const context = await browser.newContext();
+
+    return [await context.newPage(), browser];
+
+}
+
+exports.esperar = async(tiempo) => {
+    return await new Promise(r => setTimeout(r, tiempo))
+}
+
+exports.clickBotonLogin = async(page) => {
+    page = page[0]
+    await page.click('#ember12')
+}
+
+exports.verificarErrorLogin = async(page) => {
+    page = page[0]
+    await this.esperar(1000)
+    elem = await page.$('.main-error')
+    expect(await elem.innerHTML()).not.toBe('&nbsp;')
+
+}
+
+exports.cerrarNavegador = async(page) => {
+    const browser = page[1]
+    await browser.close();
+    return
+}
+
+exports.verificarLoginCorrecto = async(page, nombreUsuario) => {
+    page = page[0]
+    const elem = await page.waitForSelector(`span[title="${nombreUsuario}"]`)
+    expect(await elem.innerText()).toBe(`${nombreUsuario}`)
+}
+exports.navegarA = async(page, ruta) => {
+    page = page[0]
+    await page.goto(rutaPadre + '#/' + ruta)
+}
+
+exports.cerrarSesion = async(page, nombreUsuario) => {
+    page = page[0]
+    const elem = await page.waitForSelector(`span[title="${nombreUsuario}"]`)
+    await elem.click()
+    const elem2 = await page.waitForSelector('a[href="#/signout/"]')
+    await elem2.click()
+
+}
+
+exports.tomarCaptura = async(page, esc) => {
+    numeroCaptura += 1
+    page = page[0]
+    await page.screenshot({ path: `./${esc}/${esc}-${numeroCaptura}.png` })
+}
+
+exports.clickNuevoPost = async(page) => {
+    page = page[0];
+    await this.esperar(1000)
+    const button = await page.waitForSelector('a[href="#/editor/post/"]');
+    await button.click();
+
+}
+exports.escribirMockEnPost = async(page, title, texto) => {
+    page = page[0]
+    await this.esperar(1000)
+    const inputTitle = await page.waitForSelector('.gh-editor-title')
+    const inputText = await page.waitForSelector('p[data-koenig-dnd-droppable="true"]')
+    await inputTitle.click()
+    await inputTitle.fill(title)
+    await inputText.fill(texto);
+    await this.esperar(2000)
+}
+
+exports.publicarPost = async(page) => {
+    page = page[0];
+    const publishMenu = await page.waitForSelector('.gh-publishmenu')
+    await publishMenu.click();
+    const publishButton = await page.waitForSelector('.gh-publishmenu-button')
+    await publishButton.click();
+}
+
+exports.verificarPostPublicado = async(page, tituloPost) => {
+    page = page[0]
+    const postTitle = await page.waitForSelector(`.gh-content-entry-title:text("${tituloPost}")`)
+    const divPost = await postTitle.$('xpath=../..')
+    const badgeStatus = await divPost.$('span.gh-content-status-published')
+    expect(await badgeStatus.innerText()).toBe('PUBLISHED')
+}
