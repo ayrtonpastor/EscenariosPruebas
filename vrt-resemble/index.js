ac41f6753d34634a20995648d66f1355f9b37bae
Pedro Lizarazo <ps-lizarazo@uniandes.edu.co>
2021-05-14 21:53:57 -0500

    Last version

--- vrt-resemble/index.js
+++ vrt-resemble/index.js
@@ -1,31 +1,32 @@
 const compareImages = require("resemblejs/compareImages");
 const fs = require("mz/fs");
 let ejs = require('ejs');
-var assert = require('assert');  
+var assert = require('assert');
 
 
 const TEMPLATE_PATH = './template.ejs';
-const ESCENARIOS = ['e1','e2'] 
+const ESCENARIOS = ['e1', 'e2', 'e3', 'e11', 'e12', 'e13', 'e14'
+    , 'e16', 'e17', 'e18', 'e19']
 
 const SC_GH$3_3_3$PATH = '../Playwright/ghost3.3.0/screenshots'
 const SC_GH$3_42_5$PATH = '../Playwright/ghost3.42.5/screenshots'
 const REPORT_DIR = './reporte'
 
 const create_dir = (path) => {
-    if (!fs.existsSync(path)){
+    if (!fs.existsSync(path)) {
         fs.mkdirSync(path);
     }
 }
 
 create_dir(REPORT_DIR)
-create_dir(REPORT_DIR+'/diff')
-create_dir(REPORT_DIR+'/v3.3.0/')
-create_dir(REPORT_DIR+'/v3.42.5/')
+create_dir(REPORT_DIR + '/diff')
+create_dir(REPORT_DIR + '/v3.3.0/')
+create_dir(REPORT_DIR + '/v3.42.5/')
 
-ESCENARIOS.forEach( escenario => {
-    create_dir(REPORT_DIR+'/diff/'+escenario);
-    create_dir(REPORT_DIR+'/v3.3.0/'+escenario);
-    create_dir(REPORT_DIR+'/v3.42.5/'+escenario);    
+ESCENARIOS.forEach(escenario => {
+    create_dir(REPORT_DIR + '/diff/' + escenario);
+    create_dir(REPORT_DIR + '/v3.3.0/' + escenario);
+    create_dir(REPORT_DIR + '/v3.42.5/' + escenario);
 })
 
 async function getDiff() {
@@ -47,26 +48,25 @@ async function getDiff() {
     };
 
     let ejs_data = {
-        "escenarios" : []
+        "escenarios": []
     }
 
-    for(let escenario of ESCENARIOS)
-    {
+    for (let escenario of ESCENARIOS) {
         let ejs_escenario = {
             "name": escenario,
             "pasos": []
         }
-        e_gh_v1 = fs.readdirSync(SC_GH$3_3_3$PATH+'/'+escenario)
-        e_gh_v2 = fs.readdirSync(SC_GH$3_42_5$PATH+'/'+escenario)
+        console.log("Ejecutando comparacion escenario" + escenario)
+        e_gh_v1 = fs.readdirSync(SC_GH$3_3_3$PATH + '/' + escenario)
+        e_gh_v2 = fs.readdirSync(SC_GH$3_42_5$PATH + '/' + escenario)
         assert(e_gh_v1.length == e_gh_v2.length)
-        for(let i=0;i<e_gh_v1.length;++i)
-        {
-            const first_image_path = SC_GH$3_3_3$PATH+'/'+escenario+'/'+e_gh_v1[i];
-            const second_image_path = SC_GH$3_42_5$PATH+'/'+escenario+'/'+e_gh_v2[i];
-            
-            const result_path = REPORT_DIR+'/diff/'+escenario+'/'+e_gh_v1[i];
-            const v1_copy_path = REPORT_DIR+'/v3.3.0/'+ escenario + '/' + e_gh_v1[i]
-            const v2_copy_path = REPORT_DIR+'/v3.42.5/'+ escenario + '/' + e_gh_v2[i]
+        for (let i = 0; i < e_gh_v1.length; ++i) {
+            const first_image_path = SC_GH$3_3_3$PATH + '/' + escenario + '/' + e_gh_v1[i];
+            const second_image_path = SC_GH$3_42_5$PATH + '/' + escenario + '/' + e_gh_v2[i];
+
+            const result_path = REPORT_DIR + '/diff/' + escenario + '/' + e_gh_v1[i];
+            const v1_copy_path = REPORT_DIR + '/v3.3.0/' + escenario + '/' + e_gh_v1[i]
+            const v2_copy_path = REPORT_DIR + '/v3.42.5/' + escenario + '/' + e_gh_v2[i]
 
             assert(e_gh_v1[i] == e_gh_v2[i])
             const image_1 = await fs.readFile(first_image_path)
@@ -76,15 +76,15 @@ async function getDiff() {
                 image_2,
                 options
             );
-            console.log(data);
             await fs.writeFile(result_path, data.getBuffer());
             await fs.writeFile(v1_copy_path, image_1);
             await fs.writeFile(v2_copy_path, image_2);
             let ejs_paso = {
-                "name" : e_gh_v2[i],
-                "first_img_path" :  './v3.3.0/'+ escenario + '/' + e_gh_v1[i],
-                "second_img_path" : './v3.42.5/'+ escenario + '/' + e_gh_v2[i],
-                "diff_img_path" : './diff/'+escenario+'/'+e_gh_v1[i]
+                "name": e_gh_v2[i],
+                "first_img_path": './v3.3.0/' + escenario + '/' + e_gh_v1[i],
+                "second_img_path": './v3.42.5/' + escenario + '/' + e_gh_v2[i],
+                "diff_img_path": './diff/' + escenario + '/' + e_gh_v1[i],
+                "diff_data": data
             }
 
             ejs_escenario.pasos.push(ejs_paso)
@@ -94,8 +94,8 @@ async function getDiff() {
     return ejs_data
 }
 
-getDiff().then( ejs_data => {
-    ejs.renderFile(TEMPLATE_PATH, ejs_data, (err, html) =>{
-        fs.writeFile(REPORT_DIR+"/reporte.html", html);
+getDiff().then(ejs_data => {
+    ejs.renderFile(TEMPLATE_PATH, ejs_data, (err, html) => {
+        fs.writeFile(REPORT_DIR + "/reporte.html", html);
     })
 });
